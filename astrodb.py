dd9d5182203bb7e87355bfc017804b8a85422fc5
Éric Depagne <eric@depagne.org>
2015-01-06 19:34:22 +0200

    Added method to modify tables

--- astrodb.py
+++ astrodb.py
@@ -6,6 +6,7 @@ import sys
 
 # sql import
 import psycopg2
+from psycopg2.extensions import AsIs
 
 user = 'postgres'
 passwd = 'postgres'
@@ -13,13 +14,32 @@ host = 'localhost'
 port = 5432
 
 # Connection to the local database.
-conn = psycopg2.connect(database="Astronomy",
-                        user=user,
-                        password=passwd,
-                        host=host,
-                        port=port)
-cursor = conn.cursor()
+connection = psycopg2.connect(database="Astronomy",
+                              user=user,
+                              password=passwd,
+                              host=host,
+                              port=port)
+cursor = connection.cursor()
 
 # First, we list all tables available in the database.
 cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
 listoftables = cursor.fetchall()
+
+
+def addparameter(tablein, tableout, star, type, value):
+    """ Add a parameter for a given star
+    into the proper table
+    """
+# Get the ID of the star first
+    id = getid(tablein, star)
+    print id
+    cursor.execute("insert into temperatures (id, temperature) values ((%s), (%s) )", (id, value))
+
+    print"{0} {1} {2} {3} {4}".format(tablein, tableout, star, type, value)
+    connection.commit()
+
+
+def getid(tablein, star):
+    # Penser `a verifier que l'étoile existe.
+    cursor.execute("select id from %s where name = (%s)", (AsIs(tablein), star))
+    return cursor.fetchall()[0][0]
