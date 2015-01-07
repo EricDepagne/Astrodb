beabc909af28f30244d45f9a84bed7fe5a934b79
Éric Depagne <eric@depagne.org>
2015-01-07 11:12:59 +0200

    Check for existing values

--- astrodb.py
+++ astrodb.py
@@ -9,14 +9,12 @@ import psycopg2
 from psycopg2.extensions import AsIs
 
 user = 'postgres'
-passwd = 'postgres'
 host = 'localhost'
 port = 5432
 
 # Connection to the local database.
 connection = psycopg2.connect(database="Astronomy",
                               user=user,
-                              password=passwd,
                               host=host,
                               port=port)
 cursor = connection.cursor()
@@ -26,17 +24,47 @@ cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^
 listoftables = cursor.fetchall()
 
 
-def addparameter(tablein, tableout, star, type, value):
-    """ Add a parameter for a given star
-    into the proper table
-    """
-# Get the ID of the star first
-    id = getid(tablein, star)
-    print id
-    cursor.execute("insert into temperatures (id, temperature) values ((%s), (%s) )", (id, value))
+def addparameter(tablein, tableout, star, field, value):
+    """ Add a parameter for a given star into the proper table
 
-    print"{0} {1} {2} {3} {4}".format(tablein, tableout, star, type, value)
+    Parameters :
+    tablein : str
+
+    tableout : str
+
+    star : str
+
+    field : str
+
+    value :
+
+    Output :
+    None.
+    """
+# Get the ID of the star
+    id = getid(tablein, star)
+    present = checkentry(tableout, field, id, value)
+
+    if not present:
+        print("Inserting {0} in table {1} for star {2}".format(value, tableout, star))
+        cursor.execute("insert into temperatures (id, temperature) values ((%s), (%s) )", (id, value))
+    else :
+        print("Value {0} already in table {1} for star {2}".format(value, tableout, star))
     connection.commit()
+
+
+def checkentry(tablein, field, id, value):
+    # First, we extract all the values for a given star from the correct table
+    cursor.execute("select %s from %s where id=%s", (AsIs(field), AsIs(tablein), id))
+    result = cursor.fetchall()
+
+# Then, we´ re checking that the value to be insered is not here already.
+    present = False
+    for i, v in enumerate(result):
+        if value in v:
+            present = True
+
+    return present
 
 
 def getid(tablein, star):
