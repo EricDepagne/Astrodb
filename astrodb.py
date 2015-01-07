b86c48ebc9c7d0e868d05365f9598795add7a303
Éric Depagne <eric@depagne.org>
2015-01-07 12:25:31 +0200

    Get the DB layout

--- astrodb.py
+++ astrodb.py
@@ -22,6 +22,25 @@ cursor = connection.cursor()
 # First, we list all tables available in the database.
 cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
 listoftables = cursor.fetchall()
+# We list all the columns in each table.
+DBScheme = {}
+for i in listoftables:
+    cursor.execute("select * from %s limit 0", (AsIs(i[0]),))
+    colnames = [desc[0] for desc in cursor.description]
+    d = {i[0]: colnames}
+    DBScheme.update(d)
+
+
+def removeparameter(tablein, tableout, star, field, value):
+    id = getid(tablein, star)
+    present = checkentry(tableout, field, id, value)
+
+    if present:
+        print("Deleting {0} in table {1} for star {2}".format(value, tableout, star))
+        cursor.execute(" delete from temperatures where id=(%s) and temperature = (%s)", (id, value))
+    else:
+        print("Value {0} Not in table {1} for star {2}".format(value, tableout, star))
+    connection.commit()
 
 
 def addparameter(tablein, tableout, star, field, value):
@@ -48,17 +67,17 @@ def addparameter(tablein, tableout, star, field, value):
     if not present:
         print("Inserting {0} in table {1} for star {2}".format(value, tableout, star))
         cursor.execute("insert into temperatures (id, temperature) values ((%s), (%s) )", (id, value))
-    else :
+    else:
         print("Value {0} already in table {1} for star {2}".format(value, tableout, star))
     connection.commit()
 
 
-def checkentry(tablein, field, id, value):
-    # First, we extract all the values for a given star from the correct table
-    cursor.execute("select %s from %s where id=%s", (AsIs(field), AsIs(tablein), id))
+def checkentry(table, field, id, value):
+    # We extract all the values for a given star from the correct table
+    cursor.execute("select %s from %s where id=%s", (AsIs(field), AsIs(table), id))
     result = cursor.fetchall()
 
-# Then, we´ re checking that the value to be insered is not here already.
+# We check that the value to be insered is not here already.
     present = False
     for i, v in enumerate(result):
         if value in v:
