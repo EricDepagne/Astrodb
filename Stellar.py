f6b90df8b7806a81a54e91ea910db5a4845fa596
EricDepagne <eric@depagne.org>
2015-04-04 20:51:56 +0200

    Updated Database model

--- Stellar.py
+++ Stellar.py
@@ -1,15 +1,31 @@
-from peewee import *
+# -*- coding: utf-8 -*-
+"""
+Some stuff to query the database
+"""
+# System imports
 import base64
 import configparser
+import os
+import sys
+# peewee imports
+from peewee import Model, MySQLDatabase
+from peewee import FloatField, CharField, PrimaryKeyField, IntegerField, ForeignKeyField, DecimalField
 
 # Reading the configuration file
 Config = configparser.ConfigParser()
-Config.read('/home/eric/Science/Projets/Database/AstroConfig')
+
+configfile = os.path.expanduser("~") + '/.Astro/config'
+if os.path.isfile(configfile):
+    Config.read(configfile)
+else:
+    print('A MySQL configuration file is required. The file should be located be ~/Astro/config . For safety reasons, it should have as little permissions as possible. The file should be in the INI format, with a MySQL section that contains the username and password of the user that will connect to the database')
+    sys.exit(0)
 user = Config.get('MySQL', 'user')
 password = base64.encodebytes(bytes(Config.get('MySQL', 'passwd'), "utf-8"))
 
 database = MySQLDatabase('Stellar', **{'password': base64.decodebytes(password).decode(), 'user': user})
 # deleting the password.
+
 del password
 
 
@@ -26,13 +42,24 @@ class Star(BaseModel):
     declination = FloatField(null=True)
     epoch = CharField(null=True)
     idstar = PrimaryKeyField(db_column='idStar')
-    name = CharField(null=True)
-    pmdec = FloatField(db_column='pmDEC')
-    pmra = FloatField(db_column='pmRA')
-    right_ascension = FloatField(null=True)
+    name = CharField(unique=True)
+    pmdec = FloatField(db_column='pmDEC', null=True)
+    pmra = FloatField(db_column='pmRA', null=True)
+    rightascension = FloatField(null=True)
 
     class Meta:
         db_table = 'Star'
+
+
+class Abundance(BaseModel):
+    carbon = FloatField(db_column='Carbon', null=True)
+    oxygen = FloatField(db_column='Oxygen', null=True)
+    titanium = FloatField(db_column='Titanium', null=True)
+    idabundance = PrimaryKeyField(db_column='idAbundance')
+    starid = ForeignKeyField(db_column='starid', rel_model=Star, to_field='idstar')
+
+    class Meta:
+        db_table = 'Abundance'
 
 
 class Gravity(BaseModel):
@@ -45,16 +72,16 @@ class Gravity(BaseModel):
 
 
 class Magnitude(BaseModel):
-    b = FloatField()
-    h = IntegerField()
-    i = IntegerField()
+    bmag = DecimalField()
+    hmag = DecimalField()
     idmagnitude = PrimaryKeyField(db_column='idMagnitude')
-    j = IntegerField()
-    k = IntegerField()
-    r = IntegerField()
+    imag = DecimalField()
+    jmag = DecimalField()
+    kmag = DecimalField()
+    rmag = DecimalField()
     starid = ForeignKeyField(db_column='starid', rel_model=Star, to_field='idstar')
-    u = FloatField()
-    v = IntegerField()
+    umag = DecimalField()
+    vmag = DecimalField(max_digits=10, decimal_places=3)
 
     class Meta:
         db_table = 'Magnitude'
@@ -76,3 +103,6 @@ class Temperature(BaseModel):
 
     class Meta:
         db_table = 'Temperature'
+
+if __name__ == '__main__':
+    print("Not to be run")
